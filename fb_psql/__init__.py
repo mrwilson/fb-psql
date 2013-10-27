22c15f6c2e66e8c2e6ede72a62f9da1b057b36d4
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-10-27 11:19:33 +0000

    Error out when calls are made against a server without api key

--- fb_psql/__init__.py
+++ fb_psql/__init__.py
@@ -1,10 +1,18 @@
 from multicorn import ForeignDataWrapper
+from multicorn.utils import log_to_postgres as log
+
+import logging
 
 class FqlForeignDataWrapper(ForeignDataWrapper):
 
   def __init__(self, options, columns):
     super(FqlForeignDataWrapper, self).__init__(options, columns)
+    self.validate(options, columns)
     self.columns = columns
+
+  def validate(self, options, columns):
+    if "key" not in options:
+      log(message = "No api key given", level = logging.ERROR)
 
   def execute(self, quals, columns):
     line = {}
