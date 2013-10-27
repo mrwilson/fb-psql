947beb71e3720232220114e3cf9ca7cf82b79716
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-10-27 20:21:33 +0000

    Support tables having different names to FQL tables

--- fb_psql/__init__.py
+++ fb_psql/__init__.py
@@ -7,27 +7,38 @@ class FqlForeignDataWrapper(ForeignDataWrapper):
 
   def __init__(self, options, columns):
     super(FqlForeignDataWrapper, self).__init__(options, columns)
+
     self.validate(options, columns)
+
     self.columns = columns
-    self.key = options["key"]
+    self.key     = options['key']
+    self.table   = options['table_name']
 
   def validate(self, options, columns):
-    if "key" not in options:
-      log(message = "No api key given", level = logging.ERROR)
+    if 'key' not in options:
+      log(message = 'No api key given', level = logging.ERROR)
+
+    if 'table_name' not in options:
+      log(message = 'No corresponding FQL table', level = logging.ERROR)
 
   def handle_error(self, response):
-    error = response["error"]["message"]
+    error = response['error']['message']
     log(message = error, level = logging.ERROR)
 
+  def get_query_string(self, columns):
+    cols = ','.join(columns)
+    return 'select %s from %s where uid1=me()' % (cols, self.table)
+
   def execute(self, quals, columns):
-    params = { 'q': 'select uid2 from friend where uid1=me()', 'access_token': self.key }
 
-    r = get('https://graph.facebook.com/fql', params=params)
+    params   = { 'q': self.get_query_string(columns), 'access_token': self.key }
 
-    response = r.json()
+    log(params['q'])
 
-    if "error" in response:
+    response = get('https://graph.facebook.com/fql', params=params).json()
+
+    if 'error' in response:
       self.handle_error(response)
     else:
-      for entry in response["data"]:
+      for entry in response['data']:
         yield entry
