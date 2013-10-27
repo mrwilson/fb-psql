24d24f524f3c5523a07d25f8ac9744d285aabf1d
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-10-27 12:02:14 +0000

    Hit the api and return things. Cool.

--- fb_psql/__init__.py
+++ fb_psql/__init__.py
@@ -1,6 +1,6 @@
 from multicorn import ForeignDataWrapper
 from multicorn.utils import log_to_postgres as log
-
+from requests import get
 import logging
 
 class FqlForeignDataWrapper(ForeignDataWrapper):
@@ -9,13 +9,18 @@ class FqlForeignDataWrapper(ForeignDataWrapper):
     super(FqlForeignDataWrapper, self).__init__(options, columns)
     self.validate(options, columns)
     self.columns = columns
+    self.key = options["key"]
 
   def validate(self, options, columns):
     if "key" not in options:
       log(message = "No api key given", level = logging.ERROR)
 
   def execute(self, quals, columns):
-    line = {}
-    for column_name in self.columns:
-      line[column_name] = "example";
-    yield line
+    params = { 'q': 'select uid2 from friend where uid1=me()', 'access_token': self.key }
+
+    r = get('https://graph.facebook.com/fql', params=params)
+
+    response = r.json()
+
+    for entry in response["data"]:
+      yield entry
